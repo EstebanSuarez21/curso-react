21ed5024a449f5bb8906cb2dae25dd9974979f5b
Esteban Suarez <estebansamsuarez20@gmail.com>
2022-02-02 21:11:31 -0300

    clase 9

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,22 +2,13 @@ import React from 'react';
 import { useState } from "react";
 import "./counter.css"
 
-function Counter() {
-    const [counter, setCounter] = useState(0)
+function Counter({increment, decrement, cantidad}) {
 
-    const sumar = () => {
-        setCounter(counter + 1)
-    }
-    const restar = () => {
-        if (counter >= 1) {
-            setCounter(counter -1) 
-        }
-    }
     return (
         <div className='contador'>
-            <button className='boton' onClick={restar}>-</button>
-            <span className='numeroContador'>{counter}</span>
-            <button className='boton' onClick={sumar}>+</button>
+            <button className='boton' onClick={decrement}>-</button>
+            <span className='numeroContador'>{cantidad}</span>
+            <button className='boton' onClick={increment}>+</button>
         </div>
     )
 }
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from '../Counter/Counter';
 import "./itemDetail.css"
 
 function ItemDetail({products}) {
+    const [cantidad, setCantidad] = useState(1)
+    const increment = () => {
+        setCantidad((prevState) => prevState + 1)
+    }
+    const decrement = () => {
+        if (cantidad >= 2) {
+            setCantidad((prevState) => prevState - 1)
+        }
+    }
+
     return <div className='itemDetailContainer'>
                 <div className='detailImgContainer'>
                     <img className='detailImg' src={products.picture} alt="" />
@@ -10,8 +20,10 @@ function ItemDetail({products}) {
                 <div className='detailTextContainer'>
                     <h3 className='detailItemTitle'>{products.name}</h3>
                     <p className='detailItemDescription'>{products.description}</p>
-                    <p>{products.price}</p>
-                    <Counter/>
+                    <p>{products.price} c/u</p>
+                    <p>Su total es: {products.price * cantidad}</p>
+                    <Counter increment={increment} decrement={decrement} cantidad={cantidad}/>
+                    
                 </div>
             </div>;
 }
